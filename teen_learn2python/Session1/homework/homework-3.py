740a65309f3ca471beef3227327063824c02eeea
Tony Tallierchio <ttallierchio@gmail.com>
2023-08-04 22:10:42 -0400

    black format

--- teen_learn2python/Session1/homework/homework-3.py
+++ teen_learn2python/Session1/homework/homework-3.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-import random
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     string_one = input("Give me a random word ")
     string_two = input("Give me another random word ")
     number_one = int(input("Give me a random number "))
     number_two = int(input("Give me another random numbe "))
-    
+
     print(f"if i add the two words together i get {string_one + string_two}")
     print(f"if i add the two numbers together i get {number_one + number_two}")
