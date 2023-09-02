78adc67c24b2d379652956c0548efcd68549dc48
Tony Tallierchio <ttallierchio@gmail.com>
2023-09-02 15:11:03 -0400

    homework solves

--- teen_learn2python/Session1/homework/homework-2.py
+++ teen_learn2python/Session1/homework/homework-2.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     your_name = input("What is your name? ")
     print(f"Hello {your_name}!  It is wonderful to meet you!")
-    
